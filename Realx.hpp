b4ade133064fbd902c2c9bfc34a1eab1014639e5
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2022-04-12 19:15:24 +0300

    bug fix

--- Realx.hpp
+++ Realx.hpp
@@ -56,13 +56,14 @@ public:
 		auto t = *this;
 		const auto& kalan = t.num % v;
 		t.num /= v;
-		t.payda *= v;
 		if (kalan != 0) {
 			t.pay *= v;
 			const auto& t_pay = kalan * t.payda;
 			t.payda = v * t.payda;
 			t.pay += t_pay;
-		};
+		}
+		else
+			t.payda *= v;
 		t.sadelestir();
 		return t;
 	};
@@ -78,13 +79,14 @@ public:
 	inline auto& operator/=(const i_t& v) {
 		const auto& kalan = num % v;
 		num /= v;
-		payda *= v;
 		if (kalan != 0) {
 			pay *= v;
 			const auto& t_pay = kalan * payda;
 			payda = v * payda;
 			pay += t_pay;
-		};
+		}
+		else
+			payda *= v;
 		sadelestir();
 		return *this;
 	};
@@ -131,24 +133,29 @@ public:
 		sadelestir();
 		return *this;
 	};
-	/*
-	inline auto& operator/=(const realx<i_t, f_t>& other) {
-		const auto& kalan = num % other.num;
-		num /= v.;
-		payda *= v;
-		pay *= ;
-		if (kalan != 0) {
-			pay *= v;
-			const auto& t_pay = kalan * payda;
-			payda = v * payda;
-			pay += t_pay;
-		};
-		sadelestir();
-		return *this;
-	};
-	*/
 	friend std::ostream& operator<<(std::ostream& os, const realx<i_t, f_t>& v) {
-		return (v.pay == 0)?(os << v.num):(os << v.num << ' ' << v.pay << '/' << v.payda);
+		if (v.pay == 0)
+			return os << v.num << ".0";
+		else {
+			os << v.num << '.';
+			auto pay = v.pay;
+			auto payda = v.payda;
+			pay *= 10;
+			f_t _kalan, __kalan, ___kalan = 0;
+			bool first_digit = false;
+			do {
+				_kalan = pay % payda;
+				if (!first_digit) ___kalan = _kalan;
+				os << pay / payda;
+				pay = pay % payda;
+				pay *= 10;
+				__kalan = pay % payda;
+				first_digit = true;
+			} while(__kalan != 0 and _kalan != __kalan and __kalan != ___kalan);
+			if (__kalan == ___kalan and __kalan != 0)
+				os << "..";
+			return os;
+		};
 	};
 private:
 	void sadelestir() {
